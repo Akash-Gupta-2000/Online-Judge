0e56edcbbf9beba9f69be8ddd8ac04547367c719
Akash-Gupta-2000 <akashgupta752000@gmail.com>
2023-09-05 00:29:40 +0200

    Adding Register, Login and Logout Views

---
+++ OJ_Server/User/urls.py
@@ -0,0 +1,11 @@
+from django.urls import include, path
+from . import views
+
+urlpatterns = [
+    path('register/', views.register, name = 'Register'),
+    path('login/', views.login, name = 'Login'),
+    path('logout/', views.logout, name = 'Logout'),
+    # path('activate/<str:uidb64>/<str:token>',
+    #      views.ActivateUserMethod, name='Account Verification'),
+    # path('getProfile/', views.getProfile, name='User Profile'),
+]

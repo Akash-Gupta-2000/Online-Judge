0d29545d5e64c4655f1876533e99f95593824d69
Akash-Gupta-2000 <akashgupta752000@gmail.com>
2023-09-08 15:18:57 +0200

    Creating Problem Specific View Functions

--- OJ_Server/Problem/views.py
+++ OJ_Server/Problem/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from .models import Problem
 
-# Create your views here.
+def getAllProblems(request):
+    problems_list = Problem.objects.all()
+    problems_dict_list = []
+    for problem in problems_list:
+        tags = list(problem.tags.all().values())
+        problem_dict = model_to_dict(problem)
+        problem_dict["tags"] = tags
+        problems_dict_list.append(problem_dict)
+    # print(problems_dict_list)
+    return JsonResponse(problems_dict_list, safe=False)
+
+def getProblemsByPage(request,page_no):
+    init_count = (page_no-1)*50;
+    problems_list = Problem.objects.all()[init_count:init_count+50]
+    problems_dict_list = [model_to_dict(problem) for problem in problems_list]
+    return JsonResponse(problems_dict_list, safe=False)
+
+def getProblemById(request,id):
+    problem = Problem.objects.get(id=id)
+    tags = list(problem.tags.all().values())
+    problem_dict = model_to_dict(problem)
+    problem_dict["tags"] = tags
+    return JsonResponse(problem_dict, safe=False)
+
